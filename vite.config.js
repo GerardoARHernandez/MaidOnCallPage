f5a78346d7b2aa50a3c3162cc2b3cd2cd88c8fb1
GerardoARHernandez <gahdz14@gmail.com>
2025-03-21 12:42:59 -0600

    Pruebas 11: Vercel.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],  // 👈 Asegúrate de incluir esta línea
-  base: "/", // O usa "/" si sigue fallando
+  base: "./", // O usa "/" si sigue fallando
   build: {
     outDir: "dist",
     assetsDir: "assets"
