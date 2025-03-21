1bf43df3a4cabd755709f8e7f1b161c5c09c5e0c
GerardoARHernandez <gahdz14@gmail.com>
2025-03-21 12:28:52 -0600

    Pruebas 8: Vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "./", // Si no funciona, intenta con "/"
+  plugins: [react()],  // 👈 Asegúrate de incluir esta línea
+  base: "./", // O usa "/" si sigue fallando
   build: {
     outDir: "dist",
     assetsDir: "assets"
