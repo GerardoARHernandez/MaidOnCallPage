0027da80a0c7e4684d005225f3aa30d419582507
GerardoARHernandez <gahdz14@gmail.com>
2025-03-21 12:31:39 -0600

    Pruebas 8: Vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],  // 👈 Asegúrate de incluir esta línea
