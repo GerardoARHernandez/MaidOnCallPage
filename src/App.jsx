3f3fcd937210ef56fdcd4be8bf3e2f7cb18642b9
GerardoARHernandez <gahdz14@gmail.com>
2025-03-21 09:50:42 -0600

    Update: Cambio de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,24 @@ import GiftCard from "./pages/GiftCard";
 import Privacy from "./pages/Privacy";
 import ScrollTop from "./components/ScrollTop";
 
+// servicios
+import HouseCleaningPage from "./pages/Services/HouseCleaningPage";
+import OfficeCleaningPage from "./pages/Services/OfficeCleaningPage";
+import AppartmentCleaningPage from "./pages/Services/AppartmentCleaningPage";
+import MoveInCleaning from "./pages/Services/MoveInCleaning";
+import PostConstruction from "./pages/Services/PostConstruction";
+import ShortTermPage from "./pages/Services/ShortTermPage";
+import OneTime from "./pages/Services/OneTime";
+import SpringCleaning from "./pages/Services/SpringCleaning";
+import DeepCleaning from "./pages/Services/DeepCleaning";
+import Sanitize from "./pages/Services/Sanitize";
+import MoveOutCleaning from "./pages/Services/MoveOutCleaning";
+import PostRenovation from "./pages/Services/PostRenovation";
+import CleaningOrg from "./pages/Services/CleaningOrg";
+import Declutter from "./pages/Services/Declutter";
+import PreparationForSale from "./pages/Services/PreparationForSale";
+import StagingBasic from "./pages/Services/StagingBasic";
+import SpecialRequests from "./pages/Services/SpecialRequests";
 
 function App() {
 
@@ -33,8 +51,27 @@ function App() {
             <Route path="product/demo" element={ <GiftCard /> } />
             <Route path="join-our-team" element={ <JoinOurTeam /> } />
             <Route path="privacy" element={ <Privacy /> } />
+
+            {/* Servicios */}
+            <Route path="regular-house-cleaning-ottawa" element={ <HouseCleaningPage /> } />
+            <Route path="office-cleaning-ottawa" element={ <OfficeCleaningPage /> } />
+            <Route path="apartment-condo-cleaning-ottawa" element={ <AppartmentCleaningPage /> } />
+            <Route path="move-in-cleaning-ottawa" element={ <MoveInCleaning /> } />
+            <Route path="move-out-cleaning-ottawa" element={ <MoveOutCleaning /> } />
+            <Route path="post-construction-cleaning-ottawa" element={ <PostConstruction /> } />
+            <Route path="post-renovation-cleaning-ottawa" element={ <PostRenovation /> } />
+            <Route path="short-term-rental-airbnb-ottawa" element={ <ShortTermPage /> } />
+            <Route path="one-time-regular-cleaning" element={ <OneTime /> } />
+            <Route path="spring-cleaning-ottawa" element={ <SpringCleaning /> } />
+            <Route path="deep-house-cleaning-ottawa" element={ <DeepCleaning /> } />
+            <Route path="desinfection-services-ottawa" element={ <Sanitize /> } />
+            <Route path="cleaning-and-organization-ottawa" element={ < CleaningOrg /> } />
+            <Route path="declutter-ottawa" element={ < Declutter /> } />
+            <Route path="home-preparation-for-sale-cleaning-service-ottawa" element={ <PreparationForSale /> } />
+            <Route path="home-staging-basic-ottawa" element={ <StagingBasic /> } />
+            <Route path="add-on-special-requests" element={ <SpecialRequests /> } />
+
           </Routes>
-            <ServiceRoutes />
       </div>
       <Footer />
     </>
