f9d7933c3c53e3bfbd32dba5385aa89b41cae212
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2025-04-11 10:22:49 -0600

    Cmabio para rutas de blog

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Cambiado de "./" a "/"
+  base: "./",
   build: {
     outDir: "dist",
     assetsDir: "assets"
