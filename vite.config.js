90d060f0eab797df0d3f959c8d8b5422cc3e9a43
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2025-04-11 10:42:31 -0600

    Prueba 2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   base: "./",
   build: {
     outDir: "dist",
-    assetsDir: "assets"
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]" // Estructura consistente
+      }
+    }
   }
 });
